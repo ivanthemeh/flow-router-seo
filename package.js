3400eee96bcd2c166e0a6009fdfccf09e102dfa4
Ivanthemeh <dowerk.doitdoit@gmail.com>
2015-11-27 21:17:14 -0500

    Update package.js

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use(['mongo', 'underscore', 'jquery']);
+  api.use(['mongo', 'underscore', 'jquery', 'check']);
   api.use('kadira:flow-router@2.1.0');
 
   api.addFiles('flow-router-seo.js');
